9e9a2c8284d73f13f0426413256eeb5517825393
Sanskriti Singh <92388225+Sanskriti-26@users.noreply.github.com>
2022-09-08 12:33:05 +0530

    Create register_form.php

---
+++ register_form.php
@@ -0,0 +1,11 @@
+<html>
+    <head>registration form</head>
+    <body>
+        <form method="post" action="register_submit.php">
+            <input type="text" placeholder="Name" name="name"><br><br>
+            <input type="text" placeholder="Email" name="email"><br><br>
+            <input type="password" placeholder="Password" name="password"><br><br>
+            <input type="submit" name="submit" value="submit">
+        </form>
+    </body>
+</html>
