d854d522bcb547083577459765f835c5f69e57e6
Sanskriti Singh <92388225+Sanskriti-26@users.noreply.github.com>
2022-09-08 12:39:09 +0530

    Create login_form.php

---
+++ login_form.php
@@ -0,0 +1,10 @@
+<html>
+  <head><title>Login form</title></head>
+    <body>
+        <form method="post" action="login_submit.php">
+            <input type="text" placeholder="Email" name="email"><br><br>
+            <input type="password" placeholder="Password" name="password"><br><br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
